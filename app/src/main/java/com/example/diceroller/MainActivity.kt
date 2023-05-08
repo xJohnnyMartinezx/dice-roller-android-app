877834f5799807cfb69c8e9e64f7e0b932e48c64
Johnny <mrtnz.jn@gmail.com>
2023-05-07 19:39:51 -0500

    completed first portion of basic dice roller app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,31 @@ class MainActivity : AppCompatActivity() {
 
 //        *** SETTING UP EVENT LISTER AND DISPLAYING MSG WHEN BTN IS TAPPED ********
         rollButton.setOnClickListener {
-           val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT);
-            toast.show();
+
+//            **** THIS SHOWS A TOAST MSG WHEN BTN IS TAPPED *********
+//           val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT);
+//            toast.show();
+
+//            ****** SHOWS THE NUMBER 6 WHEN BTN IS TAPPED *********
+//            val resultTextView: TextView = findViewById(R.id.textView);
+//            resultTextView.text = "6";
+
+//            ****** CALLING rollDice FUNCTION *****
+            rollDice();
         }
+    }
+
+    private fun rollDice() {
+        val dice6 = Dice(6);
+        val diceRoll = dice6.makeRoll();
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+}
+
+class Dice(private val numSides: Int){
+    fun makeRoll(): Int{
+        return (1..numSides).random();
     }
 }
