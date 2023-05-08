3726f0780704ef4eb34fc6ce76a47714faa31412
Johnny <mrtnz.jn@gmail.com>
2023-05-08 16:40:49 -0500

    added simple unit test to app

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.diceroller
 
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +10,12 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number(){
+        val dice = Dice(6);
+        val rollResult = dice.makeRoll();
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6);
+
     }
 }
