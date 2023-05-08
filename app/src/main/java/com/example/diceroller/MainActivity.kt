3b864813697351cb9c1b33974272edfbcdcbe777
Johnny <mrtnz.jn@gmail.com>
2023-05-07 19:17:17 -0500

    created layout with an interactive btn. Setup event listener to display msg when btn id tapped.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        *** REFERENCING THE "Roll Dice!" BUTTON VIA ID ******
+        val rollButton: Button = findViewById(R.id.button);
+
+//        *** SETTING UP EVENT LISTER AND DISPLAYING MSG WHEN BTN IS TAPPED ********
+        rollButton.setOnClickListener {
+           val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
